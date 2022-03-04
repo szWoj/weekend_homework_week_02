01e3093b95a165285471dc0b295b180dc601d0e1
Szymon <wojdyla.szymon@gmail.com>
2022-03-04 15:35:21 +0000

    test song has name added

--- src/song.py
+++ src/song.py
@@ -1,2 +1,3 @@
 class Song:
-    pass
+    def __init__(self, name):
+        self.name = name
--- tests/song_test.py
+++ tests/song_test.py
@@ -2,4 +2,9 @@ import unittest
 from src.song import Song
 
 class TestSong(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.song = Song("Yellow Submarine")
+
+    def test_song_has_name(self):
+        self.assertEqual("Yellow Submarine", self.song.name)
