3e98b7e97d32d82b401d67ee0422a149b965f291
Szymon <wojdyla.szymon@gmail.com>
2022-03-04 16:14:16 +0000

    test is_room_name_unique added

--- src/room.py
+++ src/room.py
@@ -3,3 +3,12 @@ class Room:
         self.name = name
         self.list_of_guests = []
         self.list_of_songs = []
+        self.list_of_created_rooms = []
+
+    def is_room_name_unique(self, name):
+        if name not in self.list_of_created_rooms:
+            return True
+        return False
+        
+    # def create_a_room(self, room_name):
+    #     return Room(str(room_name))
--- tests/room_test.py
+++ tests/room_test.py
@@ -5,6 +5,8 @@ class TestRoom(unittest.TestCase):
     
     def setUp(self):
         self.room = Room("Room Michael")
+        self.new_possible_name = "My new room"
+        self.new_possible_name_fail = "Room Michael"
 
     def test_room_has_name(self):
         self.assertEqual("Room Michael", self.room.name)
@@ -14,3 +16,11 @@ class TestRoom(unittest.TestCase):
     
     def test_room_has_empty_list_of_songs(self):
         self.assertEqual([], self.room.list_of_songs)
+
+    def test_is_room_name_unique(self):
+        new_name = self.room.is_room_name_unique(self.new_possible_name)
+        self.assertEqual(True, new_name)
+    
+    # def test_room_has_been_created(self):
+    #     result = self.room.create_a_room(room_name)
+    #     self.assertEqual(result, Room(str(room_name)))
