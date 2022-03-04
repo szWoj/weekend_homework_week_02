f6387d6a83543e62e270fbf4a8ad7242d5df13d1
Szymon <wojdyla.szymon@gmail.com>
2022-03-04 15:37:55 +0000

    test guest has name added

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -2,4 +2,8 @@ import unittest
 from src.guest import Guest
 
 class TestGuest(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.guest = Guest("Mark Robson")
+    
+    def test_guest_has_name(self):
+        self.assertEqual("Mark Robson", self.guest.name)
