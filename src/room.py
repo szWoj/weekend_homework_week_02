4c665372bc918a3dccd89ccd8f5a613e1286d91d
Szymon <wojdyla.szymon@gmail.com>
2022-03-05 16:43:38 +0000

    test_guest_checked_in added

--- src/room.py
+++ src/room.py
@@ -3,12 +3,28 @@ class Room:
         self.name = name
         self.list_of_guests = []
         self.list_of_songs = []
-        self.list_of_created_rooms = []
+        #self.list_of_created_rooms = []
 
-    def is_room_name_unique(self, name):
-        if name not in self.list_of_created_rooms:
-            return True
-        return False
-        
+    # def is_room_name_unique(self, name):
+    #     for room in self.list_of_created_rooms:
+    #         if room.name == name:
+    #             return False
+    #     return True
+
     # def create_a_room(self, room_name):
     #     return Room(str(room_name))
+
+    def is_guest_checked_in(self, new_guest):
+        for guest in self.list_of_guests:
+            if new_guest.name == guest.name:
+                return True
+        return False
+    
+    # def check_in_a_guest(self, guest):
+    #     self.list_of_guests.append(guest)
+
+    def check_out_a_guest(self, guest):
+        pass
+
+    def add_song(self, song):
+        pass
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,25 +1,54 @@
 import unittest
 from src.room import Room
+from src.song import Song
+from src.guest import Guest
 
 class TestRoom(unittest.TestCase):
     
     def setUp(self):
-        self.room = Room("Room Michael")
-        self.new_possible_name = "My new room"
-        self.new_possible_name_fail = "Room Michael"
+        self.room1 = Room("Room Michael")
+        self.room2 = Room("Room Robbie")
+        
+        self.guest1 = Guest("Szymon Wojdyla")
+        self.guest2 = Guest("Sebastian Wojdyla")
+        
+        self.song1 = Song("Yellow Submarine")
+        self.song1 = Song("Yellow Submarine")
+        self.room1.list_of_guests = [self.guest1]
+        self.room2.list_of_guests = []
+        
+        #self.new_possible_name = "My new room"
+        #self.new_possible_name_fail = "Room Michael"
+        #self.list_of_created_rooms = [self.room]
 
     def test_room_has_name(self):
-        self.assertEqual("Room Michael", self.room.name)
+        self.assertEqual("Room Michael", self.room1.name)
 
     def test_room_has_empty_list_of_guests(self):
-        self.assertEqual([], self.room.list_of_guests)
+        self.assertEqual([], self.room2.list_of_guests)
     
     def test_room_has_empty_list_of_songs(self):
-        self.assertEqual([], self.room.list_of_songs)
+        self.assertEqual([], self.room1.list_of_songs)
 
-    def test_is_room_name_unique(self):
-        new_name = self.room.is_room_name_unique(self.new_possible_name)
-        self.assertEqual(True, new_name)
+    # def test_check_in_a_guest(self):
+    #     result = self.room1.check_in_a_guest(self.guest1)
+    #     self.assertEqual([self.guest1], result)
+    
+    def test_guest_checked_in(self):
+        result = self.room1.is_guest_checked_in(self.guest1)
+        self.assertEqual(result, True)
+
+    # def test_guest_not_checked_in(self):
+    #     result = self.room2.is_guest_checked_in(self.guest1)
+    #     self.assertEqual(result, False)
+
+    # def test_is_room_name_unique(self):
+    #     new_name = self.room.is_room_name_unique(self.new_possible_name)
+    #     self.assertEqual(True, new_name)
+    
+    # def test_is_room_name_unique_fail(self):
+    #     new_name = self.room.is_room_name_unique(self.new_possible_name_fail)
+    #     self.assertEqual(False, new_name)
     
     # def test_room_has_been_created(self):
     #     result = self.room.create_a_room(room_name)
